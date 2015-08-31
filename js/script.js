9fa23e0353d1d288aace59e5177b81cf06e250f2
Anton Bakker <anton.bakker@geocat.net>
2015-09-01 00:27:49 +0200

    fix for resize menu on oreintation change

--- js/script.js
+++ js/script.js
@@ -4,10 +4,17 @@ $(".article>.al2fb_anchor").detach().appendTo(".post-meta");
 $('[data-toggle="tooltip"]').tooltip();
 $("#registration-input").tooltip({'trigger':'focus', 'title': 'Optional details for registration'});
 
+resizeMenu();
+
+
+$(window).on('resize orientationChange', function(event) {
+    resizeMenu();
+});
+
+
+function resizeMenu(){
 var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
 var h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
-console.log("height: "+h);
-console.log("width: "+w);
 var newheight;
 if ($("#wpadminbar").length>0){
   newheight=h-104;
@@ -16,8 +23,6 @@ if ($("#wpadminbar").length>0){
   newheight=h-59;
   $(".navbar-collapse").css("max-height",newheight+"px");
 }
-
-
 if (h<300){
 
   $(".navbar-nav>li>a ").css("padding-top","10px");
@@ -26,6 +31,6 @@ if (h<300){
   $(".navbar-nav>li").css("height","40px");
   $(".navbar-nav>li:not(:has(*))").remove();
 }
+}
 
-//.navbar-fixed-top .navbar-collapse
 });
